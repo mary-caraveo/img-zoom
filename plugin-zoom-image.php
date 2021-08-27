d22da2b12c0befa4e35a136729528151ad3c2058
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-08-27 12:38:43 -0500

    feat: Creación de plugin zoom image

---
+++ plugin-zoom-image.php
@@ -0,0 +1,15 @@
+<?php
+
+//Plugin name: zoom imagenes
+//Description: Activa el zoom a las imagenes
+//version: 1.0
+//Author: Maria Caraveo
+//Author URI: github
+
+function assets_plugin() {
+	$url_raiz = plugin_dir_url(__FILE__);
+
+	wp_enqueue_style('zoom_imagen',$url_raiz.'/assets/css/estilos.css', '', '1.0', 'all');
+}
+
+add_action('wp_enqueue_scripts','assets_plugin');
