96e6c3d13b49180f768ac3f37111f386e7e33376
Guadalupe Caraveo <25497052+mary-caraveo@users.noreply.github.com>
2021-11-16 13:57:11 -0500

    fix: formato del código

--- plugin-zoom-image.php
+++ plugin-zoom-image.php
@@ -7,9 +7,8 @@
 //Author URI: github
 
 function assets_plugin() {
-	$url_raiz = plugin_dir_url(__FILE__);
-
-	wp_enqueue_style('zoom_imagen',$url_raiz.'/assets/css/estilos.css', '', '1.0', 'all');
+  $url_raiz = plugin_dir_url(__FILE__);
+  wp_enqueue_style('zoom_imagen',$url_raiz.'/assets/css/estilos.css', '', '1.0', 'all');
 }
 
 add_action('wp_enqueue_scripts','assets_plugin');
